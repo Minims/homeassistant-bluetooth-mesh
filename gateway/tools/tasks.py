4db17a715139edf68d3d2a662690db8f0ce7ff07
Dominik Berse <dominikberse@gmail.com>
2022-12-18 00:18:29 +0100

    feat: cancel tasks on uncaught exception

--- gateway/tools/tasks.py
+++ gateway/tools/tasks.py
@@ -8,7 +8,7 @@ class Tasks:
     Simple task pool
 
     TODO: This class can be extended in order to manage failed tasks.
-    Currently failed tasks are logged but otherwise ignored.
+    Currently failed tasks cause cancellation of other tasks.
     """
 
     def __init__(self):
@@ -35,8 +35,11 @@ class Tasks:
             logging.debug(f"Spawning task to {name}...")
         try:
             await task
-        except Exception as exp:  # pylint: disable=broad-except
-            logging.exception(f"Task failed: {exp}")
+        except Exception as exc:
+            logging.exception(f"Task failed: {exc}")
+            # force cancellation of all tasks
+            # depending on the configuration, this should lead to service restart
+            self._shutdown()
         if name:
             logging.debug(f"{name} completed")
 
